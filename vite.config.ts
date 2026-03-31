784b3e8741ce319580c92e6eb9381d49f8aeb4e3
Bastian <bparedesp@falabella.cl>
2026-03-30 23:26:43 -0300

    asdcasd

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
+import fs from 'node:fs';
+import path from 'node:path';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    {
+      name: 'copy-package-json',
+      writeBundle() {
+        const src = path.resolve(__dirname, 'package.json');
+        const dest = path.resolve(__dirname, 'dist', 'package.json');
+        fs.copyFileSync(src, dest);
+      },
+    },
+  ],
   define: {
     'process.env.NODE_ENV': JSON.stringify('production'),
   },
